0560a097fc8debb9e4f7ed6689119bf2f0704466
Randy Wilson <phoenixrr2113@gmail.com>
2019-01-22 19:23:35 -0500

    refactored savedlist

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class SavedList extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div className="saved-list">
-				<h3>Saved Movies:</h3>
-				{this.props.list.map((movie, i) => (
-					<span key={i} className="saved-movie">
-						{movie.title}
-					</span>
-				))}
-				<Link className="home-button" to="/">
-					Home
-				</Link>
-			</div>
-		);
-	}
-}
+const SavedList = props => (
+	<div className="saved-list">
+		<h3>Saved Movies:</h3>
+		{props.list.map((movie, i) => (
+			<span key={i} className="saved-movie">
+				{movie.title}
+			</span>
+		))}
+		<div>
+			<Link className="home-button" to="/">
+				Home
+			</Link>
+		</div>
+	</div>
+);
+
+export default SavedList;
