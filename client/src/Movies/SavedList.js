65fe4a85bf8361603623411da14fe4a643c11718
Randy Wilson <phoenixrr2113@gmail.com>
2019-01-22 19:55:57 -0500

    styled NavLink component

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const SavedList = props => (
 	<div className="saved-list">
 		<h3>Saved Movies:</h3>
 		{props.list.map((movie, i) => (
-			<span key={i} className="saved-movie">
-				{movie.title}
-			</span>
+			<NavLink
+				style={{ textDecoration: 'none' }}
+				exact
+				to={`/movies/${movie.id}`}
+				key={i}>
+				<span className="saved-movie">{movie.title}</span>
+			</NavLink>
 		))}
 		<div>
 			<Link className="home-button" to="/">
